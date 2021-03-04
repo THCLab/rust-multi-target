ad9c16c576cfab162e07977d0355bd2c856abfc3
Edyta Pawlak <edyta.pawlakk@gmail.com>
2021-03-04 09:50:56 +0100

    implement Display for KeyTypes

--- rust/jolocom_native_utils/src/did_document.rs
+++ rust/jolocom_native_utils/src/did_document.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use keri::{
     derivation::basic::Basic,
     prefix::{BasicPrefix, IdentifierPrefix, Prefix},
@@ -77,6 +79,12 @@ pub enum KeyTypes {
     X25519KeyAgreementKey2019,
     SchnorrSecp256k1VerificationKey2019,
     EcdsaSecp256k1RecoveryMethod2020,
+}
+
+impl fmt::Display for KeyTypes {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
 
 #[derive(Serialize, Deserialize)]
